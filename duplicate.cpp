0f65ac4714a665a266e558d738562e579b0a5413
bonnux <775174736@qq.com>
2018-03-21 09:35:17 +0800

    Initial commit

---
+++ duplicate.cpp
@@ -0,0 +1,18 @@
+bool duplicate(int numbers[],int length,int* duplication){
+	if (numbers == null || length<=0)
+	{
+		/* code */
+		return false;
+	}
+
+	for(int i=0; i<length; ++i){
+		if(numbers[i]<0||numbers[i]>length-1)
+			return false;
+	}
+
+	for(int i=0; i<length; ++i){
+		while(numbers[i]!=i){
+			if(numbers[i]==numbers)
+		}
+	}
+}
