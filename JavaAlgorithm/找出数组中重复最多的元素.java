ae23e423f931b2af4b26614a072422ea1ece211a
bonnux <sa517111@mail.ustc.edu.cn>
2018-05-04 16:29:28 +0800

    2018.5.4

---
+++ JavaAlgorithm/找出数组中重复最多的元素.java
@@ -0,0 +1,6 @@
+import java.util.*;
+public class MostFrequent{
+	public static int findMostFrequentInArray(int[] a){
+		
+	}
+}
--- JavaAlgorithm/求最大子数组之和.java
+++ JavaAlgorithm/求最大子数组之和.java
@@ -1,12 +1,23 @@
 public class MaxSubArr{
+	public static int max(int m, int n){
+		return m > n ? m : n;
+	}
+
 	public static int maxSubArray(int arr[]){
-		int size = arr.length;
-		int maxSum = Integer.MIN_VALUE;
-		for (int i = 0; i < size; i++ ) {
-			int sum = 0;
-			for (int j = i; j < size; j++ ) {
-				
-			}
+		int n = arr.length;
+		//End[i-1]代表arr[0]...arr[i-1]中包含arr[i-1]的最大子数组和
+		int End[] = new int[n];
+		//All[i-1]代表arr[0]...arr[i-1]中的最大子数组和(arr[i-1]包不包含都行)
+		int All[] = new int[n];
+
+		End[n-1] = arr[n-1];
+		All[n-1] = arr[n-1];
+		End[0] = All[0] = arr[0];
+		
+		for (int i = 1; i < n; i++) {
+			End[i] = max(End[i-1] + arr[i], arr[i]);
+			All[i] = max(End[i], All[i-1]);
 		}
+		return All[n-1];
 	}
 }
