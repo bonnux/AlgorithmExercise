495c6641b9c030dd934e6c62745d17741c036647
bonnux <sa517111@mail.ustc.edu.cn>
2018-05-06 08:08:31 +0800

    2018.5.6

--- JavaAlgorithm/找出数组中重复最多的元素.java
+++ JavaAlgorithm/找出数组中重复最多的元素.java
@@ -10,7 +10,7 @@ public class MostFrequent{
 		Map<Integer, Integer> m = new HashMap<>();
 		for (int i = 0; i < size; i++) {
 			if (m.containsKey(a[i])) {
-				m.put(a[i],m.get(a[i]+1));
+				m.put(a[i],m.get(a[i])+1);
 			}
 			else{
 				m.put(a[i],1);
@@ -19,5 +19,21 @@ public class MostFrequent{
 
 		//找出出现次数最多的元素
 		int most = 0;
+		Iterator iter = m.entrySet().iterator();
+		while(iter.hasNext()){
+			Map.Entry entry = (Map.Entry) iter.next();
+			int key = (Integer)entry.getKey();
+			int val = (Integer)entry.getValue();
+			if(val > most){
+				result = key;
+				most = val;
+			}
+		}
+		return result;
+	}
+
+	public static void main(String[] args) {
+		int array[] = {1,2,4,3,5,1,3,4,2,4,2,5};
+		
 	}
 }
