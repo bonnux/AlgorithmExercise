cd56362bfb2ca40816c140c8a71a88a66989c9da
bonnux <sa517111@mail.ustc.edu.cn>
2018-05-19 21:02:34 +0800

    2018.5.19

--- JavaAlgorithm/反转链表（非递归）.java
+++ JavaAlgorithm/反转链表（非递归）.java
@@ -1,5 +1,14 @@
 public void ReverseList(Node head){
 	Node pReversedHead = head;
 	Node pNode = head;
-	
+	Node pPrev = null;
+	while(pNode!=null){
+		Node pNext = pNode.next;
+		if(pNext==null)
+			pReversedHead = pNode;
+		pNode.next = pPrev;
+		pPrev = pNode;
+		pNode = pNext;
+	}
+	this.head = pReversedHead;
 }
